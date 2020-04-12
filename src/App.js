b844e24e1251b5f22aee715a18e61bf820368105
MansourMoukdad88 <m_sy@outlook.com>
2020-04-12 23:41:09 +0300

    Test Git

--- src/App.js
+++ src/App.js
@@ -13,8 +13,12 @@ const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact><Users /></Route>
-        <Route path="/places/new" exact><NewPlace /></Route>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
         <Redirect to="/" />
       </Switch>
     </Router>
