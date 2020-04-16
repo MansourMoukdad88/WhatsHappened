b581bac9c127722ee9ce5cdcc0f9789cf1b2f955
MansourMoukdad88 <m_sy@outlook.com>
2020-04-16 13:26:09 +0300

    [Add] Custom From Input component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const NewPlace = () => {
-  return <h2>NewPlace Works!</h2>
+import './NewPlace.css';
+
+import Input from '../../shared/FormElements/Input';
+
+const NewPlace = (props) => {
+  return (
+    <form className="place-form">
+      <Input element="input" type="text" label="Title"/>
+    </form>
+  )
 };
 
 export default NewPlace;
