f22fc5ddf47ad2361191ac422ee8d79cad4506e9
MansourMoukdad88 <m_sy@outlook.com>
2020-04-23 17:33:23 +0300

    [Add] input validation

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-import './NewPlace.css';
 
-import Input from '../../shared/FormElements/Input';
+import Input from '../../shared/components/FormElements/Input';
+import {VALIDATOR_REQUIRE} from '../../shared/util/validators';
+import './NewPlace.css';
 
 const NewPlace = (props) => {
   return (
     <form className="place-form">
-      <Input element="input" type="text" label="Title" validator={[]} errorText= 'Please Enter a Vaild Title'/>
+      <Input element="input" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText= 'Please Enter a Vaild Title'/>
     </form>
   );
 };
