df1cf4c3a04f44e236877ba73c14d8b8c0b5e2c2
MansourMoukdad88 <m_sy@outlook.com>
2020-04-25 03:06:27 +0300

    [Add] Auth Page and Login Form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
-import Users from './user/pages/Users';
-import NewPlace from './places/pages/NewPlace';
-import UserPlaces from './places/pages/UserPlaces';
-import UpdatePlace from './places/pages/UpdatePlace';
-import MainNavigation from './shared/components/Navigation/MainNavigation';
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+import UserPlaces from "./places/pages/UserPlaces";
+import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
     <Router>
-      <MainNavigation/>
+      <MainNavigation />
       <main>
         <Switch>
-          <Route path="/" exact><Users/></Route>
-          <Route path="/:userId/places" exact><UserPlaces/></Route>
-          <Route path="/places/new" exact><NewPlace/></Route>
-          <Route path="/places/:placeId" exact><UpdatePlace/></Route>
-          <Redirect to="/"/>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Route path="/places/:placeId">
+            <UpdatePlace />
+          </Route>
+          <Route path="/auth">
+            <Auth />
+          </Route>
+          <Redirect to="/" />
         </Switch>
       </main>
     </Router>
--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,85 +1,90 @@
-import React, {useEffect, useState} from 'react';
-import {useParams} from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-import Input from '../../shared/components/FormElements/Input';
-import Button from '../../shared/components/FormElements/Button';
-import Card from '../../shared/components/UIElements/Card';
-import {VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH} from '../../shared/util/validators';
-import {useForm} from '../../shared/hooks/form-hook';
-import './PlaceForm.css';
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import Card from "../../shared/components/UIElements/Card";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
+import { useForm } from "../../shared/hooks/form-hook";
+import "./PlaceForm.css";
 
 const DUMMY_PLACES = [
   {
-    id:'p1',
-    title: 'Empire State Building',
-    description: 'One of the most famous shy scrapers in the world!',
-    imageUrl: "https://res.cloudinary.com/drbgxq5pt/image/fetch/f_auto,q_auto:eco/https%3A%2F%2Fwww.kitano.com%2Fresourcefiles%2Fsnippet-main-img%2Fempire-state-building-in-new-york-top.jpg%3Fmimdevice%3Dmobile",
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous shy scrapers in the world!",
+    imageUrl:
+      "https://res.cloudinary.com/drbgxq5pt/image/fetch/f_auto,q_auto:eco/https%3A%2F%2Fwww.kitano.com%2Fresourcefiles%2Fsnippet-main-img%2Fempire-state-building-in-new-york-top.jpg%3Fmimdevice%3Dmobile",
     address: "20 W 34th St, New York, NY 10001, United States",
-    location:{
-      lat:40.7484405,
-      lng:-73.9856644
+    location: {
+      lat: 40.7484405,
+      lng: -73.9856644,
     },
-    creator: 'u1'
+    creator: "u1",
   },
   {
-    id:'p2',
-    title: 'Emp!!! State Building',
-    description: 'One of the most famous shy scrapers in the world!',
-    imageUrl: "https://res.cloudinary.com/drbgxq5pt/image/fetch/f_auto,q_auto:eco/https%3A%2F%2Fwww.kitano.com%2Fresourcefiles%2Fsnippet-main-img%2Fempire-state-building-in-new-york-top.jpg%3Fmimdevice%3Dmobile",
+    id: "p2",
+    title: "Emp!!! State Building",
+    description: "One of the most famous shy scrapers in the world!",
+    imageUrl:
+      "https://res.cloudinary.com/drbgxq5pt/image/fetch/f_auto,q_auto:eco/https%3A%2F%2Fwww.kitano.com%2Fresourcefiles%2Fsnippet-main-img%2Fempire-state-building-in-new-york-top.jpg%3Fmimdevice%3Dmobile",
     address: "20 W 34th St, New York, NY 10001, United States",
-    location:{
-      lat:40.7484405,
-      lng:-73.9856644
+    location: {
+      lat: 40.7484405,
+      lng: -73.9856644,
     },
-    creator: 'u2'
-  }
-]
-
+    creator: "u2",
+  },
+];
 
 const UpdatePlace = () => {
   const [isLoading, setIsLoading] = useState(true);
   const placeId = useParams().placeId;
 
-  
-  
-  const [formState, inputHandler, setFormData] = useForm({
-    title: {
-      value: '',
-      isValid: false
+  const [formState, inputHandler, setFormData] = useForm(
+    {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
     },
-    description: {
-      value: '',
-      isValid: false
-    }
-  }, false);
-  
-  const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
+    false
+  );
+
+  const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
 
   useEffect(() => {
-    if(identifiedPlace) {
-      setFormData({
-        title: {
-          value: identifiedPlace.title,
-          isValid: true
+    if (identifiedPlace) {
+      setFormData(
+        {
+          title: {
+            value: identifiedPlace.title,
+            isValid: true,
+          },
+          description: {
+            value: identifiedPlace.description,
+            isValid: true,
+          },
         },
-        description: {
-          value: identifiedPlace.description,
-          isValid: true
-        }
-      }, 
-      true);
+        true
+      );
     }
     setIsLoading(false);
   }, [setFormData, identifiedPlace]);
 
-
-  const placeUpdateSubmitHandler = event => {
+  const placeUpdateSubmitHandler = (event) => {
     event.preventDefault();
     console.log(formState.inputs);
-    
-  }
+  };
 
-  if(!identifiedPlace) {
+  if (!identifiedPlace) {
     return (
       <div className="center">
         <Card>
@@ -88,38 +93,41 @@ const UpdatePlace = () => {
       </div>
     );
   }
-  if(isLoading) {
+  if (isLoading) {
     return (
       <div className="center">
         <h2>Loading...</h2>
       </div>
     );
-  };
+  }
   return (
     <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
-      <Input 
-        id="title" 
-        element="input" 
-        type="text" 
-        label="Title" 
-        validators={[VALIDATOR_REQUIRE()]} 
-        errorText="Please enter a valid title." 
-        onInput={inputHandler} 
-        initialValue={formState.inputs.title.value} 
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+        onInput={inputHandler}
+        initialValue={formState.inputs.title.value}
         initialValid={formState.inputs.title.isValid}
       />
-      <Input 
-        id="description" 
-        element="textarea" 
-        label="Description" 
-        validators={[VALIDATOR_MINLENGTH(5)]} 
-        errorText="Please enter a valid description (min. 5 characters)." 
-        onInput={inputHandler} 
-        initialValue={formState.inputs.description.value} 
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (min. 5 characters)."
+        onInput={inputHandler}
+        initialValue={formState.inputs.description.value}
         initialValid={formState.inputs.description.isValid}
       />
-      <Button type="submit" disabled={!formState.isValid}>UPDATE EVENT</Button>
-    </form>)
+      <Button type="submit" disabled={!formState.isValid}>
+        UPDATE EVENT
+      </Button>
+    </form>
+  );
 };
 
 export default UpdatePlace;
